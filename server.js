ae80a1c6633d7b1f855b2a750f08674efadfc9bc
Krish Patel <115217085+krishpatel1610@users.noreply.github.com>
2024-10-20 23:41:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,45 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb+srv://aditya:aditya@cluster0.ntkoj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology:true
 })
-.then(() => console.log("Connected to DB"))
-.catch(err => console.error("Database connection error:", err));
+    .then(() => console.log("Connect to DB"))
+    .catch(console.error);
 
 const Todo = require('./models/Todo');
 
-// Get all todos
 app.get('/todos', async (req, res) => {
-    try {
-        const todos = await Todo.find();
-        res.json(todos);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    const todos = await Todo.find();
+
+    res.json(todos);
 });
 
-// Create a new todo
 app.post('/todo/new', async (req, res) => {
-    try {
-        const todo = new Todo({
-            text: req.body.text
-        });
-        await todo.save();
-        res.json(todo);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    const todo = await new Todo({
+        text: req.body.text
+    });
+
+    todo.save();
+
+    res.json(todo);
 });
 
-// Delete a todo
 app.delete('/todo/delete/:id', async (req, res) => {
-    try {
-        const result = await Todo.findByIdAndDelete(req.params.id);
-        res.json(result);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json(result);
 });
 
-// Toggle completion status of a todo
+
 app.get('/todo/complete/:id', async (req, res) => {
     try {
         const todo = await Todo.findById(req.params.id);
@@ -70,5 +57,5 @@ app.get('/todo/complete/:id', async (req, res) => {
     }
 });
 
-// Start the server
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
